8af4117ff88d1d065c69274b9ad894dcedf98e95
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-08-24 22:41:40 -0300

    Add Post resolver

--- src/graphql/resources/post/post.resolvers.ts
+++ src/graphql/resources/post/post.resolvers.ts
@@ -1,6 +1,7 @@
 import {GraphQLResolveInfo} from "graphql";
 import {DbConnection} from "../../../interfaces/DbConnectionInterface";
 import {PostInstance} from "../../../models/PostModel";
+import {Transaction} from "sequelize";
 
 export const postResolvers = {
 
@@ -37,5 +38,41 @@ export const postResolvers = {
         }
     },
 
+    Mutation: {
+        createPost: (parent, {input}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+            return db.sequelize.transaction((t: Transaction) => {
+               return db.Post.create(input, {transaction: t});
+            });
+        },
+        updatePost: (parent, {id, input}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Post
+                    .findById(id)
+                    .then((post: PostInstance) => {
+                        if (!post) {
+                            throw new Error(`Pos with id ${id} not found`);
+                        }
 
+                        return post.update(input, {transaction: t});
+                    });
+            });
+        },
+        deletePost: (parent, {id, input}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Post
+                    .findById(id)
+                    .then((post: PostInstance) => {
+                        if (!post) {
+                            throw new Error(`Pos with id ${id} not found`);
+                        }
+
+                        return post.destroy( {transaction: t})
+                            .then(post => !!post);
+                    });
+            });
+        }
+
+    }
 };
