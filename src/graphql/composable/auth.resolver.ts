027178cd051f2eeb5f5d4b7d56cba083804910a3
Marcos Almeida <marcos.almeida@abacomm.com.br>
2018-08-27 20:47:38 -0300

    Avoid variable name colision

--- src/graphql/composable/auth.resolver.ts
+++ src/graphql/composable/auth.resolver.ts
@@ -6,7 +6,7 @@ export const authResolver: ComposableResolver<any, ResolverContext> =
     (resolver: GraphQLFieldResolver<any, ResolverContext>): GraphQLFieldResolver<any, ResolverContext> => {
 
     return (parent, args, context: ResolverContext, info) => {
-        if (context.user || context.authorization) {
+        if (context.authUser || context.authorization) {
             return resolver(parent, args, context, info);
         }
 
--- src/interfaces/ResolverContextInterface.ts
+++ src/interfaces/ResolverContextInterface.ts
@@ -4,5 +4,5 @@ import {AuthUser} from "./AuthUserInterface";
 export interface ResolverContext {
     db?: DbConnection;
     authorization?: string;
-    user?: AuthUser
+    authUser?: AuthUser
 }
