f298be63988b07ab34228a341f50ded7c458e551
Marcos Almeida <marcos.almeida@abacomm.com.br>
2018-08-29 19:57:42 -0300

    Add UserLoader

---
+++ src/graphql/dataloaders/UserLoader.ts
@@ -0,0 +1,12 @@
+import {UserInstance, UserModel} from "../../models/UserModel";
+
+export class UserLoader {
+
+    static batchUsers(User: UserModel, ids: number[]): Promise<UserInstance[]> {
+        return Promise.resolve(
+            User.findAll({
+                where: {id: {$in: ids}}
+            })
+        );
+    }
+}
